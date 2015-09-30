a2224f3e90dded65fc2ac11f9f5639d2665d4dd8
李枨煊 <i@chengxuan.li>
2015-09-30 18:13:03 +0800

    完成：Github登录

---
+++ application/controllers/Github/Login.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Github登录
+ *
+ * @package Controller
+ * @author  chengxuan <chengxuan@staff.weibo.com>
+ */
+class Github_OauthController extends Yaf_Controller_Abstract {
+
+    
+    //Github登录
+    public function indexAction() {
+        $url = \Model\Github::showLoginUrl();
+        return $this->redirect($url);
+    }
+    
+}
