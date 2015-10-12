46ddf29efca57f5ff230d9642afd3e3ce14ab4fe
李枨煊 <i@chengxuan.li>
2015-10-12 20:38:04 +0800

    完成：博客基础方法

---
+++ application/library/Model/Blog.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * 博客配置
+ *
+ * @package Model
+ * @author  chengxuan <i@chengxuan.li>
+ */
+namespace Model;
+class Blog extends Abs {
+
+    /**
+     * 主键ID
+     *
+     * @var string
+     */
+    protected static $_primary_key = 'uid';
+    
+    /**
+     * 数据表
+     *
+     * @var string
+     */
+    protected static $_table = 'blog';
+    
+    /**
+     * 写入一个博客数据
+     * 
+     * @param int   $uid  用户UID
+     * @param array $data 博客配置
+     * 
+     * @return \int
+     */
+    static public function create($uid, array $data) {
+        $data = self::encodeData($data);
+        return self::db()->insert(['uid' => $uid, 'data' => $data], true, true);
+    }
+    
+    
+    /**
+     * 更新一条数据
+     * 
+     * @param int   $uid  用户UID
+     * @param array $data 博客配置
+     * 
+     * @return \int
+     */
+    static public function update($uid, array $data) {
+        $data = self::encodeData($data);
+        return self::db()->wAnd(['uid' => $uid])->upadte(['data' => $data], true);
+    }
+    
+    /**
+     * 保存数据
+     * 
+     * @param int   $uid  用户UID
+     * @param array $data 博客配置
+     * 
+     * @return \int
+     */
+    static public function save($uid, $data) {
+        $db_data = self::show($uid);
+        if(!$db_data) {
+            $result = self::create($uid, $data);
+        }
+        if($db_data || empty($result)) {
+            $result = self::update($uid, $data);
+        }
+        return $result;
+    }
+    
+    /**
+     * 获取一条数据
+     * 
+     * @param int   $uid  用户UID
+     * 
+     * @return array
+     */
+    static public function show($uid) {
+        $result = parent::show($uid);
+        isset($result['data']) && $result['data'] = self::decodeData($result['data']);
+        return $result;
+    }
+    
+    /**
+     * 编码数据
+     * 
+     * @param array $data
+     * 
+     * @return string
+     */
+    static public function encodeData(array $data) {
+        empty($data) && $data = new \stdClass();
+        return \Comm\Json::encode($data);
+    }
+    
+    /**
+     * 解码数据
+     * 
+     * @param string $data
+     * 
+     * @return \array
+     */
+    static public function decodeData($data) {
+        if($data) {
+            $result = \json_decode($data, true);
+        } else {
+            $result = array();
+        }
+        return $result;
+    }
+    
+    
+    
+} 
