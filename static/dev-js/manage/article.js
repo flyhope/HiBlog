83642a90b3eb54dee3b61ac910c9a013b1489744
李枨煊 <i@chengxuan.li>
2015-10-27 14:04:52 +0800

    完成：可视化编辑器

--- static/dev-js/manage/article.js
+++ static/dev-js/manage/article.js
@@ -1,4 +1,17 @@
-CKEDITOR.replace( 'editor1', {
-	"baseHref" : "http://libs.useso.com/",
-	"customConfig" : ''
-} );
+$(function() {
+
+	
+	CKEDITOR.replace( 'editor1', {
+		"font_defaultLabel" : "Simsun",
+		"font_names" : "Simsun;SimHei;KaiTi;Microsoft Yahei;Arial;Times New Roman;Verdana;",
+		"fontSize_defaultLabel" : "14px",
+		"toolbar" :        [
+	        ["Styles", "Format", "Font", "FontSize"],
+	        ["Bold", "Italic", "Underline", "Strike", "-", 'TextColor','BGColor'],
+	        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock', "-", 'NumberedList', 'BulletedList', '-', 'Link', "Unlink"],
+	        ["Image", "Flash", "Table", "HorizontalRule", "SpecialChar", "Iframe"],
+	        ["Maximize", "-" ,"Source"]
+	    ]
+	} );
+	
+})
