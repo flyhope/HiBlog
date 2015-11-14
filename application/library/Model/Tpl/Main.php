8dded9443b3dbf0e1930bb08e7443ee40c5abb34
李枨煊 <i@chengxuan.li>
2015-11-14 23:33:46 +0800

    添加模板入口

--- application/library/Model/Tpl/Main.php
+++ application/library/Model/Tpl/Main.php
@@ -14,6 +14,19 @@ class Main extends \Model\Abs {
      */
     protected static $_table = 'tpl_main';
     
-    
+    /**
+     * 获取指定用户的主题 
+     * 
+     * @param int     $uid          要操作的UID
+     * @param boolean $show_default 是否获取默认模板
+     * 
+     * @return \array
+     */
+    public function userTpls($uid = false, $show_default = true) {
+        $uid || $uid = \Yaf_Registry::get('current_uid');
+        $where_uid = $show_default ? [$uid, 0] : $uid;
+        $db = self::db()->wAnd(['user_id'=>$where_uid])->order('id', SORT_DESC);
+        return $db->limit()->fetchAll();
+    }
     
 }
