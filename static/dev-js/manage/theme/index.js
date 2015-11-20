776c649c2c67fb64b1a30ca0b4de95878e2ad0d9
李枨煊 <i@chengxuan.li>
2015-11-20 13:18:54 +0800

    完成：删除主题

--- static/dev-js/manage/theme/index.js
+++ static/dev-js/manage/theme/index.js
@@ -4,8 +4,8 @@ $(function() {
 	$("#modal-theme-copy").on("show.bs.modal", function(event) {
 		//复制模板
 		var button = $(event.relatedTarget);
-		var theme_node = button.parents("[node-type=theme-node]");
-		var theme_id = theme_node.data("theme-id");
+		var $theme_node = button.parents("[node-type=theme-node]");
+		var theme_id = $theme_node.data("theme-id");
 		
 		$(this).find("[name=alias_id]").val(theme_id);
 	});
@@ -13,9 +13,16 @@ $(function() {
 	//复制模板表单提交事件
 	$("#form-theme-copy").ajaxSubmit();
 	
-	
-	$("#theme-container").delegate("[action-type=use]", "click", function() {
-		
+	//删除主题
+	$("#theme-container").delegate("[action-type=destroy]", "click", function() {
+		var $theme_node = $(this).parents("[node-type=theme-node]");
+		var theme_id = $theme_node.data("theme-id");
+		var href = $CONFIG + "aj/manage/theme/destroy";
+		$.post(href, {"id" : theme_id}, function (o) {
+			$.ajaxCallback(o, function() {
+				$theme_node.fadeOut();
+			});
+		});
 	});
 	
 });
