4819a874af95a2abea26a558addd935ce2a7c769
chengxuan <i@chengxuan.li>
2015-12-20 16:53:05 +0800

    完成：文章页

--- static/builder/theme-default/hiblog.js
+++ static/builder/theme-default/hiblog.js
@@ -1,15 +1,21 @@
 $(function() {
 
+	//响应式设计交互
 	function resizeSides() {
 	    if ($(window).width() < 992) {
-	        $("#sides").removeClass("in");
-	        $("body").removeClass("md");
+	        $("#sides").removeClass("in"); //右侧栏
+	        $("body").removeClass("md");	//body右空白区域
 	        
 	    } else {
-	    	$("#sides").collapse("show");
-	    	$("body").addClass("md");
+	    	$("#sides").collapse("show"); //右侧栏
+	    	$("body").addClass("md");	//body右空白区域
 	    };
 	}
 	$(window).bind("resize", resizeSides);
-	resizeSides();
+	
+	//加载右侧栏
+	$("#sidebar").load("./block/sidebar.html", resizeSides);
+	
+
+	
 });
