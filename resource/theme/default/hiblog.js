11bc15680a430a38411b74e2461c894b12039074
flyhope <i@chengxuan.li>
2015-12-21 23:13:02 +0800

    Update hiblog.js

--- resource/theme/default/hiblog.js
+++ resource/theme/default/hiblog.js
@@ -22,11 +22,19 @@ $(function() {
 	
 	//加载高亮组件
 	if($("pre[class*=brush]").size()) {
-		var script_load_total = 0;
-		var scriptload_callback = function() {
-			console.log(script_load_total);
-			if(++script_load_total > 1) {
-				console.log("execute");
+		//替换加载器路径占位
+		function SyntaxHighlighterPath() {
+			var args = arguments,
+			result = [];
+			for(var i = 0; i < args.length; i++) {
+				result.push(args[i].replace('@', 'http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/'));
+			}
+			return result;
+		};
+		
+		$("head").append('<link type="text/css" rel="stylesheet" href="http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/styles/shCore.css" /><link type="text/css" rel="stylesheet" href="http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/styles/shThemeDefault.css" />');
+		$.getScript('http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/shCore.js', function() {
+			$.getScript('http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/shAutoloader.js', function() {
 				SyntaxHighlighter.autoloader.apply(null, SyntaxHighlighterPath(
 					'applescript            @shBrushAppleScript.js',
 					'actionscript3 as3      @shBrushAS3.js', 
@@ -54,21 +62,9 @@ $(function() {
 					'xml xhtml xslt html    @shBrushXml.js'
 				));
 				SyntaxHighlighter.all();
-			}
-		};
+			});
+		});
 		
-		function SyntaxHighlighterPath() {
-			var args = arguments,
-			result = [];
-			for(var i = 0; i < args.length; i++) {
-				result.push(args[i].replace('@', 'http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/'));
-			}
-			return result;
-		};
-		
-		$("head").append('<link type="text/css" rel="stylesheet" href="http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/styles/shCore.css" /><link type="text/css" rel="stylesheet" href="http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/styles/shThemeDefault.css" />');
-		$.getScript('http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/shCore.js', scriptload_callback);
-		$.getScript('http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/shAutoloader.js', scriptload_callback);
 	}
 	
 });
