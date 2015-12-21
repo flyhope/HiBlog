cce59f1acec974947233de6eb8faf96ad09af8c2
flyhope <i@chengxuan.li>
2015-12-21 22:42:46 +0800

    Update hiblog.js

--- resource/theme/default/hiblog.js
+++ resource/theme/default/hiblog.js
@@ -20,6 +20,48 @@ $(function() {
 	}
 	$("#sidebar").load(src_sidebar, resizeSides);
 	
-
+	//加载高亮组件
+	if($("pre").size()) {
+		$("head").append('<link type="text/css" rel="stylesheet" href="http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/styles/shCore.css" /><link type="text/css" rel="stylesheet" href="http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/styles/shThemeDefault.css" />');
+		$.getScript('http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/shCore.js');
+		$.getScript('http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/shAutoloader.js');
+		
+		function SyntaxHighlighterPath() {
+		    var args = arguments,
+		    result = [];
+		    for(var i = 0; i < args.length; i++)
+		        result.push(args[i].replace('@', 'http://apps.bdimg.com/libs/SyntaxHighlighter/3.0.83/scripts/'));
+		    return result
+		};
+		SyntaxHighlighter.autoloader.apply(null, SyntaxHighlighterPath(
+		    'applescript            @shBrushAppleScript.js',
+		    'actionscript3 as3      @shBrushAS3.js', 
+		    'bash shell             @shBrushBash.js', 
+		    'coldfusion cf          @shBrushColdFusion.js',
+		    'cpp c                  @shBrushCpp.js',
+		    'c# c-sharp csharp      @shBrushCSharp.js', 
+		    'css                    @shBrushCss.js', 
+		    'delphi pascal          @shBrushDelphi.js', 
+		    'diff patch pas         @shBrushDiff.js', 
+		    'erl erlang             @shBrushErlang.js',
+		    'groovy                 @shBrushGroovy.js', 
+		    'java                   @shBrushJava.js',
+		    'jfx javafx             @shBrushJavaFX.js', 
+		    'js jscript javascript  @shBrushJScript.js',
+		    'perl pl                @shBrushPerl.js',
+		    'php                    @shBrushPhp.js', 
+		    'text plain             @shBrushPlain.js', 
+		    'py python              @shBrushPython.js',
+		    'ruby rails ror rb      @shBrushRuby.js',
+		    'sass scss              @shBrushSass.js',
+		    'scala                  @shBrushScala.js',
+		    'sql                    @shBrushSql.js', 
+		    'vb vbnet               @shBrushVb.js',
+		    'xml xhtml xslt html    @shBrushXml.js'
+		    ));
+		    SyntaxHighlighter.all();	
+		});
+		
+	}
 	
 });
