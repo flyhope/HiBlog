db4b423396642fea7cee4fe1045f04c7c0d434ec
flyhope <i@chengxuan.li>
2015-12-21 22:54:16 +0800

    Update hiblog.js

--- resource/theme/default/hiblog.js
+++ resource/theme/default/hiblog.js
@@ -21,7 +21,7 @@ $(function() {
 	$("#sidebar").load(src_sidebar, resizeSides);
 	
 	//加载高亮组件
-	if($("pre").size()) {
+	if($("pre.brush").size()) {
 		var script_load_total = 0;
 		var scriptload_callback = function() {
 			if(++script_load_total > 1) {
