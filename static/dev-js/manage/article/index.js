abc2d4b658f0c5282779485f5b3075a23b1ba828
chengxuan <i@chengxuan.li>
2016-01-11 19:13:00 +0800

    完成：编辑确认项

--- static/dev-js/manage/article/index.js
+++ static/dev-js/manage/article/index.js
@@ -9,8 +9,10 @@ $(function() {
 		$.post(href, {"id" : $tr.data("id")}, $.ajaxCallbackDefault);
 	}).delegate("[action-type=delete]", "click", function() {
 		//代理删除事件
-		var $tr = $(this).parents("tr:first");
 		var href = $CONFIG.path + "aj/manage/article/destroy";
-		$.post(href, {"id" : $tr.data("id")}, $.ajaxCallbackDefault);
+		var article_id = $(this).parents("tr:first").data("id");
+		$.confirm("确定要删除吗? (ID:" + article_id + ")", "确认", function() {
+			$.post(href, {"id" : article_id}, $.ajaxCallbackDefault);
+		});
 	});
 });
